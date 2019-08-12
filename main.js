d54cc6ded81c01f57f229a85f6a1a900647b3869
Jordan Young <young.desin@gmail.com>
2019-08-12 00:10:35 -0400

    replaced main.js

--- main.js
+++ main.js
@@ -1,64 +1,64 @@
-    /* Three.JS script */
-    var container,
-            renderer,
-        scene,
-        camera,
-        mesh,
-        start = Date.now(),
-        fov = 30;
-        
-    window.addEventListener( 'load', function() {
-    //grab the container from the DOM
-    container = document.getElementById('container');
+var container, 
+    renderer, 
+    scene, 
+    camera, 
+    mesh, 
+    start = Date.now(),
+    fov = 30;
+
+window.addEventListener( 'load', function() {
+
+    // grab the container from the DOM
+    container = document.getElementById( "container" );
     
-    //create a scene
+    // create a scene
     scene = new THREE.Scene();
+
+    // create a camera the size of the browser window
+    // and place it 100 units away, looking towards the center of the scene
+    camera = new THREE.PerspectiveCamera( 
+        fov, 
+        window.innerWidth / window.innerHeight, 
+        1, 
+        10000 );
+    camera.position.z = 100;
+    camera.target = new THREE.Vector3( 0, 0, 0 );
+
+    scene.add( camera );
+
+material = new THREE.ShaderMaterial( {
+      uniforms: { 
+        time: { // float initialized to 0
+            type: "f", 
+            value: 0.0 
+        }
+    },
+    vertexShader: document.getElementById( 'vertexShader' ).textContent,
+    fragmentShader: document.getElementById( 'fragmentShader' ).textContent
+} );
     
-    //create a camera the size of the browser window
-    //place it 100 units away, looking towards the center of the scene
-    camera = new THREE.PerspectiveCamera(
-        fov,
-      container.clientWidth / container.clientHeight,
-      1,
-      10000
-      );
-    camera.position.z=100;
-    
-    //create wireframe material
-    material = new THREE.ShaderMaterial({
-        uniforms: {
-            tExplosion: {
-                type:'t',
-                value: THREE.TextureLoader('')
-            },
-            time: {
-                type: 'f',
-                value: 0.0
-            }
-        },
-        vertexShader: document.getElementById( 'vertexShader' ).textContent,
-      fragmentShader: document.getElementById( 'fragmentShader' ).textContent
-      } );
-      
-    //create a sphere and assign the material
-    mesh = new THREE.Mesh(
-        new THREE.IcosahedronGeometry(20, 4),
-      material
-      );
+    // create a sphere and assign the material
+    mesh = new THREE.Mesh( 
+        new THREE.IcosahedronGeometry( 20, 4 ), 
+        material 
+    );
     scene.add( mesh );
     
-    //create the renderer and attach it to the DOM
+    // create the renderer and attach it to the DOM
     renderer = new THREE.WebGLRenderer();
-    renderer.setSize(container.clientWidth, container.clientHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize( window.innerWidth, window.innerHeight );
     
-    container.appendChild(renderer.domElement);
-    
+    container.appendChild( renderer.domElement );
+
     render();
-    } );
+
+} );
+
+function render() {
+    // uniform update
+    material.uniforms[ 'time' ].value = .00025 * ( Date.now() - start );
+    // let there be light
+    renderer.render( scene, camera );
+    requestAnimationFrame( render );
     
-    function render() {
-        material.uniforms[ 'time' ].value = .00025 * ( Date.now() - start);
-        renderer.render( scene, camera );
-      requestAnimationFrame ( render );
-    }
+}
